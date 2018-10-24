5fb9d7d29fd5ce64b8a3bd26a4ba8a63c6ea66ec
Liam O'Neill <l.oneill20@nuigalway.ie>
2018-10-24 14:26:39 +0100

    got Jitpack stuff working

---
+++ src/main/java/ie/nuigalway/it/oneill/liam/ct417/assignment1/b/StudentRegistrationProgram.java
@@ -0,0 +1,13 @@
+package ie.nuigalway.it.oneill.liam.ct417.assignment1.b;
+
+import ie.nuigalway.it.oneill.liam.ct417.assignment1.a.*;
+
+/**
+ *
+ * @author Liam
+ */
+public class StudentRegistrationProgram {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
